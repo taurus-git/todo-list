187c5fc2109284ef6c1d2031037090f1e4a3ff33
taurus-git <t.1113@ya.ru>
2019-07-15 10:10:10 +0300

    Trying to use insertAdjacentHTML for creation new tasks.

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 var tasksHolder = document.getElementById('tasks-holder');
 var newTaskButton = document.getElementById('new-task-button');
 
-
-
-
 function addNewTask(){
-    var newTask = document.createElement('li');
-    var newTaskContent = document.getElementById('new-task-content').value;
-    newTask.innerHTML = newTaskContent;
+    var newTask = document.createElement('li'); //создать контейнер для li
+    var newTaskContent = document.getElementById('new-task-content').value; //взять значение в инпуте
+    tasksHolder.insertAdjacentHTML("afterBegin", "<b>1111</b>");
 
-    tasksHolder.appendChild(newTask);
+    //newTask.innerHTML = newTaskContent; // положить в контейнер содержимое инпута
+
+    //tasksHolder.appendChild(newTask); //вставить контейнер с содержимым в документ
 };
 
 newTaskButton.addEventListener("click", addNewTask);
