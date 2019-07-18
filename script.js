b76c70e31c5fca241d26458f00526f9e2aec4a01
Vyacheslav <vyacheslav.isupov@catenamedia.com>
2019-07-18 10:40:03 +0300

    Reformat HTML structure. Added event on keyup (add new task by Enter press in addition of mouseclick).

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 "use strict";
-var tasksHolder = document.getElementById('tasks-holder');
-var newTaskButton = document.getElementById('new-task-button');
+var tasksHolder = document.getElementById('tasks-holder'); //get holder for all tasks
+var newTaskButton = document.getElementById('new-task-button'); //create button for creation new task
+var taskInput = document.getElementById('new-task-content');
 
 function addNewTask(){
-    var newTask = document.createElement('li'); //создать контейнер для li
-    var newTaskContent = document.getElementById('new-task-content').value; //взять значение в инпуте
-    tasksHolder.insertAdjacentHTML("afterBegin", "<b>1111</b>");
-
-    //newTask.innerHTML = newTaskContent; // положить в контейнер содержимое инпута
-
-    //tasksHolder.appendChild(newTask); //вставить контейнер с содержимым в документ
+    var newTask = document.createElement('li'); //create holder for <li>
+    newTask.className = "list-item";
+    var todoText = document.getElementById('new-task-content').value; //get input value
+    newTask.innerHTML = todoText; // insert into <li> input value
+    tasksHolder.appendChild(newTask); // insert tew element
 };
 
 newTaskButton.addEventListener("click", addNewTask);
-
+taskInput.addEventListener('keyup', function (e) {
+    if(e.keyCode === 13){
+        addNewTask();
+    }
+});
