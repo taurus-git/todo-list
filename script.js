a67458f65ee7932730be4588c0d328383a729f29
Vyacheslav <vyacheslav.isupov@catenamedia.com>
2019-07-24 09:59:01 +0300

    Reformat js structure. Added verification on empty input. Added style for done task. Added default empty value in html markup.

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 "use strict";
-var tasksHolder = document.getElementById('tasks-holder'); //get holder for all tasks
-var newTaskButton = document.getElementById('new-task-button'); //create button for creation new task
-var taskInput = document.getElementById('new-task-content');
+var tasksHolder = document.getElementById("tasks-holder"); //get holder for all tasks
+var newTaskButton = document.getElementById("new-task-button"); //create button for creation new task
+var taskInput = document.getElementById("new-task-content");
 
 function addNewTask(){
-    var newTask = document.createElement('li'); //create holder for <li>
-    newTask.className = "list-item";
-    var todoText = document.getElementById('new-task-content').value; //get input value
-    newTask.innerHTML = todoText; // insert into <li> input value
-    tasksHolder.appendChild(newTask); // insert tew element
-};
+    var task = document.createElement("li"); //create holder for <li>
+    task.className = "list-item";
 
+    var todoText = taskInput.value; //get input value
+
+    if(!todoText){
+        return;
+    }
+
+    task.innerHTML = todoText; // insert into <li> input value
+    taskInput.value = ""; // insert into <li> input an empty value
+    tasksHolder.appendChild(task); // insert tew element
+}
+
+//Add new task on mouse or button click
 newTaskButton.addEventListener("click", addNewTask);
-taskInput.addEventListener('keyup', function (e) {
+taskInput.addEventListener("keyup", function (e) {
     if(e.keyCode === 13){
         addNewTask();
     }
 });
+
+//add "done" action and style
+task.addEventListener("click", addTextStyle);
+function addTextStyle() {
+    if(task.classList){
+        task.classList.toggle("line-through")
+    };
+}
